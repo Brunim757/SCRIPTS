b7b5a4c14955a6205a16527b75121e6017b4b3bb
Brunim757 <brunotiquelme@gmail.com>
2026-02-04 10:37:09 -0300

    Update Discord.lua

--- Discord.lua
+++ Discord.lua
@@ -7,7 +7,7 @@
 local Config = {
     Webhook = "https://discord.com/api/webhooks/1468450683832242373/FjjQxp03SB8sk1J-sBUsWbr2LYjd9AJEFppolEArJjlYL0WgKXQz-7GzrTsNoaqXJeaf",
     Footer = "Blox Fruits Ultimate Monitor",
-    Icon = "https://i.imgur.com/8pK6F9n.png",
+    Icon = "https://imgur.com/gallery/blox-fruits-NsWk189#Vi1TUFQ",
 
     FruitCooldown = 5,      -- segundos
     BeliMilestone = 1_000_000,
